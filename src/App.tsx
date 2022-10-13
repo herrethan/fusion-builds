1adc0548900ae966f3623433601918635796b693
Ethan Herr <herrethan@gmail.com>
2022-10-13 10:01:15 -0400

    space

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a className="App-link" href="/storybook">Storybook →</a>
-
         <h2 style={{ paddingBottom: 30}}>Fusion Builds</h2>
         <a className="App-link" href="fusion-2.3.0.tgz">fusion-2.3.0.tgz</a>
       </header>
