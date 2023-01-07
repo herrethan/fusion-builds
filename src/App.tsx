1026a228c73bb4ad2f4c43c831d422da9bc10d15
Ethan Herr <herrethan@gmail.com>
2023-01-07 08:52:16 -0500

    try out fusion design system builds

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a className="App-link" href="/fusion-design-system">Fusion Design System →</a>
         <a className="App-link" href="/storybook">Storybook →</a>
         <h2 style={{ paddingBottom: 30}}>Fusion Builds</h2>
         <a className="App-link" href="fusion-2.3.0.tgz">fusion-2.3.0.tgz</a>
         <a className="App-link" href="fusion-2.3.1.tgz">fusion-2.3.1.tgz</a>
         <a className="App-link" href="fusion-2.3.3.tgz">fusion-2.3.3.tgz</a>
         <a className="App-link" href="fusion-2.4.0.tgz">fusion-2.4.0.tgz</a>
+        <a className="App-link" href="fusion-2.6.9.tgz">fusion-2.6.9.tgz</a>
       </header>
     </div>
   );
