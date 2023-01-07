b7c6eb0e38618b6387371226f329903c9633dc46
Ethan Herr <herrethan@gmail.com>
2023-01-07 08:58:06 -0500

    remove broken fds

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a className="App-link" href="/fusion-design-system">Fusion Design System →</a>
+        {/* <a className="App-link" href="/fusion-design-system">Fusion Design System →</a> */}
         <a className="App-link" href="/storybook">Storybook →</a>
         <h2 style={{ paddingBottom: 30}}>Fusion Builds</h2>
         <a className="App-link" href="fusion-2.3.0.tgz">fusion-2.3.0.tgz</a>
